cc2ee68390ba3d7547a4d49ad403d1bf547982a5
apoorv-rane2 <apoorv.rane@neosoftmail.com>
2021-10-26 08:57:22 +0530

    Add project files.

---
+++ DocumentManagement.Web/Pages/LeaveRequestDenied.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocumentManagement.Web.Pages
+{
+    public class LeaveRequestDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
